edb92c2e85ec7a947239792dbb3762b53bfbbb25
TH-Raju <rajukhan894200@gmail.com>
2022-12-23 11:46:14 +0600

    maximum complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { useState } from 'react';
 import './App.css';
 import PieCharts from './PieCharts';
 
 function App() {
+  const [show, setShow] = useState(false);
+  let val = show ? 'Hide Original' : 'Show Original';
+
+
   return (
     <div className="App">
+      <h2>Chart</h2>
       <PieCharts></PieCharts>
-      <img src={require('../src/img/chart.png')} style={{ height: '100%', width: '100%' }} alt="" />
+      <button style={{ padding: '8px 15px', fontWeight: 'bolder', backgroundColor: 'cyan', color: 'black', fontSize: '15px', marginBottom: '20px' }} onClick={() => setShow(!show)}>{val}</button>
+      <img src={require('../src/img/chart.png')} style={{ height: '900px', width: '900px', display: `${show ? 'block' : 'none'}` }} alt="" />
     </div>
   );
 }
--- src/PieCharts.jsx
+++ src/PieCharts.jsx
@@ -1,5 +1,9 @@
-import React, { PureComponent } from 'react';
-import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
+import React from 'react';
+import { PieChart, Pie, Cell } from 'recharts';
+
+
+const COLORS = ['#eddb3d', '#f42eea', '#2bc4c1', '#2e4870', '#85c613'];
+const COLOR = ['#2e4870', '#2bc4c1', '#eddb3d', '#85c613', '#f42eea'];
 
 const data01 = [
     { name: 'Group A', value: 450 },
@@ -8,42 +12,41 @@ const data01 = [
     { name: 'Group D', value: 380 },
     { name: 'Group E', value: 200 },
 ];
-const COLORS = ['#eddb3d', '#f42eea', '#2bc4c1', '#2e4870', '#85c613'];
-const COLOR = ['#2e4870', '#2bc4c1', '#eddb3d', '#85c613', '#f42eea'];
-
-
-
-
-
-const RADIAN = Math.PI / 180;
-const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
-    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-    const x = cx + radius * Math.cos(-midAngle * RADIAN);
-    const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
-    return (
-        <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="center">
-            {`${(percent * 100).toFixed(0)}%`}
-        </text>
-    );
-};
-
-
 
 const data02 = [
-    { name: 'A1', value: 140 },
-    { name: 'A2', value: 120 },
-    { name: 'B1', value: 100 },
-    { name: 'B2', value: 100 },
-    { name: 'C1', value: 100 },
+    { name: 'A1', value: 270 },
+    { name: 'A2', value: 280 },
+    { name: 'B1', value: 160 },
+    { name: 'B2', value: 260 },
+    { name: 'C1', value: 140 },
+
+];
+const data03 = [
+    { name: 'A2', value: 5 },
+
+];
+
+const data04 = [
+    { name: 'C1', value: 45 },
+
+];
+const data05 = [
+    { name: 'B1', value: 10 },
+
+];
+const data06 = [
+    { name: 'B2', value: 25 },
+
+];
+const data07 = [
+    { name: 'A1', value: 15 },
 
 ];
 
 
 
+
 const PieCharts = () => {
-
-
     return (
         <div>
 
@@ -72,21 +75,85 @@ const PieCharts = () => {
 
                 </Pie>
 
-                <Pie data={data02}
+                <Pie data={data05}
                     dataKey="value"
                     cx="50%"
                     cy="50%"
                     labelLine={false}
-                    label={renderCustomizedLabel}
-                    innerRadius={160}
-                    outerRadius={380}
-                    fill="#82ca9d"
+                    label
+                    innerRadius={150}
+                    outerRadius={280}
+                    paddingAngle={0}
+                    fill="#f42eea"
+                // indigo
                 >
-                    {data01.map((entry, index) => (
-                        <Cell key={`cell-${index}`} fill={COLOR[index % COLORS.length]} />
-                    ))}
-
                 </Pie>
+                <Pie data={data07}
+                    dataKey="value"
+                    cx="50%"
+                    cy="50%"
+                    labelLine={false}
+                    innerRadius={150}
+                    outerRadius={320}
+                    label
+                    paddingAngle={45}
+                    fill="#85c613"
+                // jam
+                ></Pie>
+                <Pie data={data06}
+                    dataKey="value"
+                    cx="50%"
+                    cy="50%"
+                    labelLine={false}
+                    innerRadius={150}
+                    outerRadius={380}
+                    label
+                    paddingAngle={130}
+                    fill="#eddb3d"
+                // holud
+                >
+                </Pie>
+                <Pie data={data04}
+                    dataKey="value"
+                    cx="50%"
+                    cy="50%"
+                    labelLine={false}
+                    innerRadius={150}
+                    outerRadius={420}
+                    label
+                    paddingAngle={180}
+                    fill="#2bc4c1"
+                //cyan
+                >
+                </Pie>
+                <Pie data={data03}
+                    dataKey="value"
+                    cx="50%"
+                    cy="50%"
+                    labelLine={false}
+                    innerRadius={150}
+                    outerRadius={420}
+                    label
+                    paddingAngle={270}
+                    fill="#FFFFFF"
+
+                // blue
+                >
+                </Pie>
+                <Pie data={data03}
+                    dataKey="value"
+                    cx="50%"
+                    cy="50%"
+                    labelLine={false}
+                    innerRadius={150}
+                    outerRadius={220}
+                    label
+                    paddingAngle={270}
+                    fill="#2e4870"
+                // blue
+                >
+                </Pie>
+
             </PieChart>
 
         </div>
